b8e7f1d3bdcb0bb968e92ad8de20936e80eefa51
Kevin Ho <kevinho@redairship.com>
2022-05-25 04:23:03 +0800

    feature(user_profile): add profile manager

---
+++ src/user_profile/user_profile.manager.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../data-source";
+import { Profile } from "../user_profile/entity/User_profile"
+
+const profileRepository = AppDataSource.getRepository(Profile);
+
+export async function getProfileById(id: number): Promise<Profile | null> {
+    const profile = await profileRepository.findOne({
+        where: {
+            id: id,
+        },
+        relations: {
+            expense: true,
+        }
+    })
+
+    return profile;
+}
