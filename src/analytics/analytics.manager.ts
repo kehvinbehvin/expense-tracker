0c7c6b92047ab1734ff7361badd029aa56158db3
Kevin Ho <kevinho@redairship.com>
2022-05-26 01:16:19 +0800

    refactor(analytics): remove unused code

--- src/analytics/analytics.manager.ts
+++ src/analytics/analytics.manager.ts
@@ -1,10 +1,8 @@
 import { AppDataSource } from "../data-source";
 import { Expense } from "../expense/entity/Expense";
 import { Profile } from "../user_profile/entity/User_profile"
-import { createQueryBuilder } from "typeorm"
 
 const expenseRepository = AppDataSource.getRepository(Expense);
-const profileRepository = AppDataSource.getRepository(Profile);
 
 export async function getTransactionsByDuration(profile: Profile, start_date: string, end_date: string): Promise<Expense[]> {
     // Query database for all expenses of a profile between 2 months
