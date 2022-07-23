c29d815f2d30f0a11a2d5d2bc1fced9ec260f4d2
kevin <hokevin78@gmail.com>
2022-07-23 17:12:08 +0800

    fix: heroku database ssl connection

--- src/data-source.ts
+++ src/data-source.ts
@@ -42,14 +42,17 @@ function getDataSource() {
         return new DataSource({
             type: "postgres",
             url: DATABASE_URL,
-            username: DB_USERNAME_POSTGRES,
-            password: DB_PASSWORD_POSTGRES,
-            database: DB_DATABASE_POSTGRES,
             synchronize: true,
             logging: true,
             entities: [ExpUser, Expense, Profile, Payable, Receivable],
             migrations: [],
             subscribers: [],
+            ssl: true,
+            extra: {
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            }
         })
     }
 }
