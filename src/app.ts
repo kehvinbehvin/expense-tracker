8a95cdf256e40391acadaad3939e3e8e44665eff
Kevin Ho <kevinho@redairship.com>
2023-04-29 14:20:35 +0800

    feat(supabase): update to new db

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,48 @@
-import express  from "express";
+// Dependencies
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import "reflect-metadata"
+import logger from "./utils/logger/src/logger";
+
+// Database Connection
 import { AppDataSource } from "./data-source";
+
+// Routes (Import custom routes here)
+// eg: Import serviceRoute from "./<service>/<RouteFile>"
 import userRoutes from "./user/user.routes"
 import expenseRoutes from "./expense/expense.routes";
 import transactionsRoutes from "./analytics/analytics.routes";
 import payableRoutes from "./payable/payable.routes";
 import receivableRoutes from "./receivable/receivable.routes";
+
+// Middleware
 import routeLogger from "./utils/logger/src/routeLogger.middleware";
 import errorHandler from "./utils/error_handling/errorHandler.middleware";
-import logger from "./utils/logger/src/logger";
+var cors = require('cors')
 
-const app = express();
+const app: Express = express();
+const port = process.env.PORT;
 
+var corsOptions = {
+    origin: ["http://localhost:3000"],
+    optionsSuccessStatus: 200 
+  }
+
+// Middleware before module routing
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routeLogger);
 
+// Initialise custom routes here
+// eg: serviceRoutes(app)
 userRoutes(app);
 expenseRoutes(app);
 payableRoutes(app);
 receivableRoutes(app);
 transactionsRoutes(app);
 
+// Middleware after module routing
 app.use(errorHandler);
 
 AppDataSource.initialize().then(async () => {
@@ -27,6 +50,6 @@ AppDataSource.initialize().then(async () => {
 }).catch(error => console.log(error))
 
 
-app.listen(3000, () => {
-    logger.log("info","Application listening at port 3000")
+app.listen(port, () => {
+    logger.log("info",`Server is running on port ${port}`);
 })
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,54 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import fs from "fs"
+
+// Import custom schemas here
+// eg: import { User } from "./<service>/<schemaFile>"
 import { User } from "./user/entity/User"
 import { Expense } from "./expense/entity/Expense"
 import { Profile } from "./user_profile/entity/User_profile"
 import { Payable } from "./payable/entity/Payable"
 import { Receivable } from "./receivable/entity/Receivable";
-require("dotenv").config();
 
-const {DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    synchronize: true,
-    logging: false,
-    entities: [User, Expense, Profile, Payable, Receivable],
-    migrations: [],
-    subscribers: [],
-})
+const {
+    LOCAL_DATABASE_URL,
+    NODE_ENV,
+    DATABASE_URL
+    } = process.env
+
+function getDataSource() {
+    if (NODE_ENV === "development") {
+        return new DataSource({
+            type: "postgres",
+            url: LOCAL_DATABASE_URL,
+            synchronize: true,
+            logging: true,
+            entities: [User, Expense, Profile, Payable, Receivable],
+            subscribers: [],
+            migrations: [],
+            ssl: {
+                ca: fs.readFileSync('./private/local-certificate.crt').toString()
+            },
+        })
+    } else {
+        return new DataSource({
+            type: "postgres",
+            url: DATABASE_URL,
+            synchronize: true,
+            logging: true,
+            entities: [User, Expense, Profile, Payable, Receivable],
+            migrations: [],
+            subscribers: [],
+            ssl: {
+                ca: process.env.SSL_CERT
+            },
+        })
+    }
+}
+
+const AppDataSource = getDataSource()
+
+export { AppDataSource }
